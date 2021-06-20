7bd7dff4f8fe53a5e5b112c9211414cd36910cb6
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2021-06-20 17:26:35 +0100

    fix: add navbar and routes.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
+import {Switch, Route, NavLink, Redirect} from 'react-router-dom';
+import PokemonList from './containers/PokemonList';
+import Pokemon from './containers/Pokemon';
+import PokeLogo from './assets/images/International_Pokémon_logo-300x110.webp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <NavLink to={'/'}>
+          <img src={PokeLogo} width="100" height="50" />
+        </NavLink>
+      </nav>
+      <Switch>
+        <Route path={'/'} exact component={PokemonList} />
+        <Route path={'/pokemon/:pokemon'} exact component={Pokemon} />
+        {/* /pokemon/test
+         /pokemon/anything
+        */}
+        <Redirect to={'/'} />
+      </Switch>
     </div>
   );
 }
